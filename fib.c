115762ceee1dd24ec95eb21e2f3f1687c306a1c6
Daniel Taylor <daniel.taylor6456@gmail.com>
2025-03-17 18:04:18 -0400

    commit

--- fib.c
+++ fib.c
@@ -5,7 +5,6 @@ int fib(int num1){
     if (num1 == 0) {
         return 0;
     }
-
     else if ((num1 == 1) || (num1 == 2)) {
         return 1;
     }
@@ -13,7 +12,6 @@ int fib(int num1){
         return (fib(num1-1) + fib(num1-2));
     }
 }
-
 int main(void) {
     for(int i = 0; i <= 15; i++) {
         printf("%d\n", fib(i));
